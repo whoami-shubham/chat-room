1ed0fb6a886adcd02bfc303645ec4a5264d82799
whoami-shubham <shubhamkumarjha0013@gmail.com>
2019-09-13 02:56:01 +0530

    merge

--- server/index.js
+++ server/index.js
@@ -1,24 +1,51 @@
-const server = require('ws').Server;
-let s = new server({port:3000});
+const WebSocket = require('ws');
+let s = new WebSocket.Server({port:3000});
 var client=0;
-
-s.on('connection',function(ws) {
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+users = {};
+s.on('connection',function(ws,req) {
 	client++;
+	const ip = req.connection.remoteAddress;
+	users[ip] = getRandomColor();
+	var color = users[ip];
 	ws.on('message',function(message){
+         color = users[ip];
 		console.log('message :',message);
-		if(message.trim()=="hi"){
-			 ws.send("hello from the other side !");
-		}
-		else if(message.trim()=="bye"){
-			 ws.send("see You Later !");
-		}
-        else{
-        	 ws.send(message);
-        }
+		s.clients.forEach(function e(user){
+			if(user!=ws){
+				user.send('<span style='+`color:${color};`+'>'+ ip +' : </span>'+message);
+			}
+		});
+
 	})
 	ws.on('close',function(){
+		//this.send('<span style="color:yellow;">'+'<span style="color:red;">' + ip+ '</span>'+  ' Leaved the chat room .</span>');
+		s.clients.forEach(function e(user){
+			if(user!=ws){
+				user.send('<span style="color:yellow;">'+'<span style='+ `color:${color}` + '>' + ip+ '</span>'+  ' Leaved the chat room .</span>');
+			}
+		});
 		client--;
-		console.log('Total connections is '+client);
+		delete users[ip];
+		console.log('We lost a Client. Total connections is '+client);
 	})
-	console.log('New Client Connected. Total clients:'+client);
+	s.clients.forEach(function e(user){
+
+			if(user!=ws){
+				user.send('<span style="color:white;">'+'<span style='+ `color:${color}` + '>' + ip+ '</span>'+  ' Entered the chat room .</span>');
+
+			}
+			else{
+				user.send('<span style="color:white;">'+'<span style='+ `color:${color}` +'> You : '+'</span>'+  ' Entered the chat room .</span>');
+
+			}
+		});
+	console.log('New User Connected. Total Connections:'+client);
 })
